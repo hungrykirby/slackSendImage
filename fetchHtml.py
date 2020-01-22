a536e86cdbb8fd316ed3790219e411e9d9c25818
hungrykirby <katsuki8hanawa6@gmail.com>
2020-01-22 23:35:45 +0900

    よしなに時間と気圧を取得

--- fetchHtml.py
+++ fetchHtml.py
@@ -13,10 +13,22 @@ class HtmlFetcher:
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
     trs = soup.select("div#div_table > table > tr")
+    list_time_pressure = []
     for tr in trs:
       tds = tr.select("td")
-      # 2hours ago
-      print(tds)
+      print(tds[0].get_text().isdigit())
+      if tds[0].get_text().isdigit() and tds[8].get_text().replace('\xa0', ''):
+        list_time_pressure.append(
+          {
+            'time': int(tds[0].get_text()),
+            'pressure': tds[8].get_text()
+          }
+        )
     print("---")
-    # print(trs)
+    if len(list_time_pressure) == 0:
+      return None
+    else:
+      sorted_time_pressures = sorted(list_time_pressure, key=lambda x:-x['time'])
+      nearest = sorted_time_pressures[0]
+    print(nearest)
     print("---")
