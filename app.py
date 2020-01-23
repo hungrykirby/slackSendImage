d547e44f2b7ea55133b46fad37eefc6ac9f0e72b
hungrykirby <katsuki8hanawa6@gmail.com>
2020-01-23 22:47:24 +0900

    取得できなかった時に終了するように

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ def main():
   si = slackItem.SlackItem()
   fetched_html = fetchHtml.HtmlFetcher(None)
   fetched_html_data = fetched_html.fetch_pressure_from_jma(None)
+  if fetched_html_data is None:
+    return False
   text = str(fetched_html_data[0]['time']) + '時の気圧は'
   text += fetched_html_data[0]['pressure'] + 'hPaです'
   si.post_text(text)
